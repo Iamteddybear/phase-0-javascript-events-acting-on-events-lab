0533b934accac5f30500681928e830748955db3b
ned <nedschultz@outlook.com>
2023-01-09 14:07:51 +1000

    commit tree bafe7ffb2a8bd841b06d6a8fce4123b0842d3896

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+console.log(dodger);
+
+document.addEventListener("keydown", function (target) {
+    console.log(target);
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+
+    if (target.key === "ArrowLeft" && left >= 10) {
+        dodger.style.left = `${left - 10}px`;
+    }
+
+    if (target.key === "ArrowRight" && left <= 350) {
+        dodger.style.left = `${left + 10}px`;
+    }
+});
+moveDodgerLeft(left)
