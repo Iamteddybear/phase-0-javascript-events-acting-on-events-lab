cdeb81ed0c3e196b3cad6b0afddd1ae7bcfc928e
ned <nedschultz@outlook.com>
2023-01-10 12:44:02 +1000

    finished assignment

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 // Your code here
 const dodger = document.getElementById("dodger");
 
-function moveDodgerLeft(position) {
-    if (position >= 0) {
-        dodger.style.left = `${position - 10}px`;
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
     }
 }
 
 function moveDodgerRight(position) {
-    if (position <= 360) {
-        dodger.style.left = `${position + 10}px`;
+    const leftNumbers = dodger.style.left.replace("px", "");
+
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`;
     }
 }
 
-document.addEventListener("keydown", function (target) {
-    const leftNumbers = dodger.style.left.replace("px", "");
+document.addEventListener("keydown", function (event) {
 
-    const leftPosition = parseInt(leftNumbers, 10);
-
-    if (target.key === "ArrowLeft") {
-        moveDodgerLeft(leftPosition);
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
     }
 
-    if (target.key === "ArrowRight") {
-        moveDodgerRight(leftPosition);
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
     }
 });
