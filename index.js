0faf93ea399fdeb25c9c51a22f79919e4fa7f9b2
ned <nedschultz@outlook.com>
2023-01-10 12:21:08 +1000

    test

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(position) {
+    if (position >= 0) {
+        dodger.style.left = `${position - 10}px`;
+    }
+}
+
+function moveDodgerRight(position) {
+    if (position <= 360) {
+        dodger.style.left = `${position + 10}px`;
+    }
+}
+
+document.addEventListener("keydown", function (target) {
+    const leftNumbers = dodger.style.left.replace("px", "");
+
+    const leftPosition = parseInt(leftNumbers, 10);
+
+    if (target.key === "ArrowLeft") {
+        moveDodgerLeft(leftPosition);
+    }
+
+    if (target.key === "ArrowRight") {
+        moveDodgerRight(leftPosition);
+    }
+});
